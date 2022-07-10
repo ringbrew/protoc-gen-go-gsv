9e4a1c09a7309da22d5d7ed3270a28f78d4cdfa6
ringbrew <ringxu666@163.com>
2022-07-10 15:18:03 +0800

    feat: try jsonpb

--- domain/service_impl.go
+++ domain/service_impl.go
@@ -59,4 +59,5 @@ import "google.golang.org/protobuf/encoding/protojson"
 func (x *[[$m.GoIdent.GoName]]) MarshalJSON() ([]byte, error) {
 	return protojson.MarshalOptions{EmitUnpopulated: true}.Marshal(x)
 }
+[[end]]
 `
